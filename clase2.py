8436c7db5f2caec426419b3c497ef005f5378356
yppabonf <yuletsyp.pabonf@unac.edu.co>
2022-05-26 14:06:13 -0500

    class2

---
+++ clase2.py
@@ -0,0 +1,24 @@
+print("Hola mundo")
+
+x=10
+y = 5.5
+
+# print(type(x)) # int = integer 
+# print(type(y)) # float = floating point number
+# print(x.__class__) # int = integer 
+# print(y.__class__) # float = floating point number
+
+
+print(f"{type(10)=}") 
+print(f"{type(5.5)=}") 
+print(f"{type('hola')=}")
+print(f"{type(True)=}") 
+print(f"{type([1,2,3,4])=}")
+print(f"{type((1,2,3,4))=}") #tupla no cambia los valores
+print(f"{type({10})=}") # set conjunto matematico sin repetirlos
+print(f"{type({'1':1})=}") #dict dictionary :json
+
+ 
+ 
+
+ 
