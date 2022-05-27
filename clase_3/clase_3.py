18799056913bda2ffb0c2aefdedbb1dd9fa22cc2
yppabonf <yuletsyp.pabonf@unac.edu.co>
2022-05-27 14:53:56 -0500

    clase 3

--- clase_3/clase_3.py
+++ clase_3/clase_3.py
@@ -15,4 +15,18 @@ def sum_multi(numero1, numero2):
 numero1, numero2 = 10, 20
 result = sum_multi(numero1, numero2)
 print(result)
-print(type(result))
+# print(type(result))
+print(1,2,sum_multi(1,2))
+print(3,4,sum_multi(3,4))
+
+
+nums1 = (1,2,3,4)
+nums2 = (1,2,3,4,5,6,7,8,9)
+
+print(f"{len(nums1)=}")
+print(f"{len(nums2)=}")
+
+for numero in nums2:
+    print(f"{numero=}")
+
+
